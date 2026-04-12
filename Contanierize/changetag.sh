3489f2df08e4b36cdaa84ab12ecf29138bff7359
Pavankumar-git31 <pavankumarkothala31@gmail.com>
2026-04-12 10:56:55 +0530

    Update changetag.sh

--- Contanierize/changetag.sh
+++ Contanierize/changetag.sh
@@ -5,9 +5,9 @@ DOCKER_TAG="v1"
 
 echo "Updating Docker config in scripts..."
 # update dockerbuild.sh
-sed -i "s|^DOCKER_USER=.*|DOCKER_USER=\"$DOCKER_USER\"|" dockerbuild.sh
-sed -i "s|^DOCKER_VERSION=.*|DOCKER_VERSION=\"$DOCKER_TAG\"|" dockerbuild.sh
+sed -i "s|^DOCKER_USER=.*|DOCKER_USER=\"$DOCKER_USER\"|" Contanierize/dockerbuild.sh
+sed -i "s|^DOCKER_VERSION=.*|DOCKER_VERSION=\"$DOCKER_TAG\"|" Contanierize/dockerbuild.sh
 # update dockerpush.sh
-sed -i "s|^DOCKER_USER=.*|DOCKER_USER=\"$DOCKER_USER\"|" dockerpush.sh
-sed -i "s|^DOCKER_VERSION=.*|DOCKER_VERSION=\"$DOCKER_TAG\"|" dockerpush.sh
+sed -i "s|^DOCKER_USER=.*|DOCKER_USER=\"$DOCKER_USER\"|" Contanierize/dockerpush.sh
+sed -i "s|^DOCKER_VERSION=.*|DOCKER_VERSION=\"$DOCKER_TAG\"|" Contanierize/dockerpush.sh
 
